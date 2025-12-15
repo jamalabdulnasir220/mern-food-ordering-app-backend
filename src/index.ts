a19233c089c89d3f08d77f364864b789a5057a44
unknown <jamalnasirone52@gmail.com>
2025-12-15 19:43:02 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./config/db.js";
+import mongoose from "mongoose";
 import authRouter from "./routes/authRouter.js";
+import { v2 as cloudinary } from "cloudinary";
+
+mongoose
+  .connect(`${process.env.MONGODB_CONNECTION_STRING}/foodOrderingApp` as string)
+  .then(() => {
+    console.log("Connected to the database");
+  });
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+//   api_key: process.env.CLOUDINARY_API_KEY as string,
+//   api_secret: process.env.CLOUDINARY_API_SECRET as string,
+//   // secure_distribution: 'mydomain.com',
+//   // upload_prefix: 'https://api-eu.cloudinary.com'
+// });
 
 const app = express();
 
@@ -19,13 +34,17 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api", authRouter);
 
-connectDB()
-  .then(() => {
-    console.log("Database connection successful");
-    app.listen(PORT, () => {
-      console.log(`Server listening on http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log("Error connecting to the database", error);
-  });
+// connectDB()
+//   .then(() => {
+//     console.log("Database connection successful");
+//     app.listen(PORT, () => {
+//       console.log(`Server listening on http://localhost:${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("Error connecting to the database", error);
+//   });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
