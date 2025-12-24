86592f299c6ebe60af9192f0625a5bd362ff2abe
unknown <jamalnasirone52@gmail.com>
2025-12-24 06:39:48 +0000

    order router tweaks

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -7,6 +7,20 @@ const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const STRIPE_ENDPOINT_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
 
+export const getMyOrder = async (req: Request, res: Response) => {
+  try {
+    const orders = await Order.find({ user: req.userId })
+      .populate("restaurant")
+      .populate("user");
+    
+    res.json(orders)
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 type CheckoutSessionRequest = {
   cartItems: {
     menuItemId: string;
--- src/routes/orderRouter.ts
+++ src/routes/orderRouter.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { jwtCheck, parseJwt } from '../middleware/auth.js'
-import { createCheckoutSession, stripeWebhookHandler } from '../controllers/orderController.js'
+import { createCheckoutSession, getMyOrder, stripeWebhookHandler } from '../controllers/orderController.js'
 
 const orderRouter = express.Router()
 
 orderRouter.post("/checkout/create-checkout-session", jwtCheck, parseJwt, createCheckoutSession)
 
 orderRouter.post("/checkout/webhook", stripeWebhookHandler)
+
+orderRouter.get("/", jwtCheck, parseJwt, getMyOrder)
 
 export default orderRouter
